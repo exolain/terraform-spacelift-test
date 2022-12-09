75093f36a9a9828ec700d401c36192b995aba2b6
c23383a <natalia.marin@experian.com>
2022-12-09 11:21:50 -0600

    update additional required variables

--- vars.tf
+++ vars.tf
@@ -1,3 +1,7 @@
 variable worker_pool_id {}
 variable security_groups {}
 variable vpc_subnets {}
+variable worker_pool_config {}
+variable worker_pool_private_key {}
+variable worker_pool_security_groups {}
+variable worker_pool_subnets {}
