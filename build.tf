f46ee982a9d75f2c6c5d2aa789b19c3839c30546
c23383a <natalia.marin@experian.com>
2022-12-09 12:19:47 -0600

    update variables for proxy configuration

--- build.tf
+++ build.tf
@@ -18,6 +18,10 @@ module "my_workerpool" {
   configuration = <<-EOT
     export SPACELIFT_TOKEN="${var.worker_pool_config}"
     export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
+    export HTTPS_PROXY="http://usaeast-proxy.us.experian.eeca:9595"
+    export HTTP_PROXY="http://usaeast-proxy.us.experian.eeca:9595"
+    export NO_PROXY="169.254.169.254,localhost,127.0.0.1,.experiannet.corp,.aln.experian.com,.experian.eeca,.mck.experian.com,.sch.experian.com,.experian.local,.experian.corp,.gdc.local,.41web.internal,10.8.60.100,10.188.14.49,10.156.225.164,metadata.google.internal,metadata,10.188.14.56"
+   
   EOT
 
   max_size          = 1
