e5eba31ed6656ddce73371b1541974f93cba4c33
c23383a <natalia.marin@experian.com>
2022-12-09 12:04:04 -0600

    update default vars

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable worker_pool_id {}
-variable worker_pool_security_groups {}
-variable worker_pool_subnets {}
+variable worker_pool_security_groups { default = ["sg-0ea1d0eaa183a1e69"]}
+variable worker_pool_subnets { default = ["subnet-0d25d35fb9c253d7e"]}
 variable worker_pool_config {}
 variable worker_pool_private_key {}
